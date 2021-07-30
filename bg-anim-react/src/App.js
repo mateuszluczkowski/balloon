95b1108710c469b823c276509b73e9f18854e449
Mateusz Łuczkowski <mateuszluczkowski@gmial.com>
2021-07-30 12:49:01 +0200

    change functions to class

--- bg-anim-react/src/App.js
+++ bg-anim-react/src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Animated from "./Animated";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="balloon-animation">
+          <Animated
+            className="flying-cloud"
+            src="/cloud.png"
+            ratioX="0.35"
+            ratioY="0.36"
+          />
+          <Animated
+            className="flying-cloud"
+            src="/cloud.png"
+            ratioX="0.2"
+            ratioY="0.23"
+          />
+          <Animated
+            className="flying-cloud"
+            src="/cloud.png"
+            ratioX="0.35"
+            ratioY="0.17"
+          />
+          <Animated
+            className="flying-cloud"
+            src="/cloud.png"
+            ratioX="0.23"
+            ratioY="0.27"
+          />
+          <Animated
+            className="flying-cloud"
+            src="/cloud.png"
+            ratioX="0.14"
+            ratioY="0.45"
+          />
+          <Animated
+            className="flying-cloud"
+            src="/cloud.png"
+            ratioX="0.16"
+            ratioY="0.07"
+          />
+          <Animated
+            className="flying-cloud"
+            src="/balloon.png"
+            ratioX="0.07"
+            ratioY="0.36"
+          />
+        </div>
       </header>
     </div>
   );
